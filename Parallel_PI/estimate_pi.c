29b05edc7a4dceb1692b703cc6b4343eb246c829
elee_totient <ericlee0803@yahoo.com>
2016-09-25 15:14:44 -0400

    Commit Parallel_PI

--- Parallel_PI/estimate_pi.c
+++ Parallel_PI/estimate_pi.c
@@ -13,27 +13,31 @@ int main(int argc, char* argv[])
 	double z;
 	double pi;
 	/* initialize random numbers */
-  	srand(SEED);
-  
+  	int carray[24];
+
 	#pragma omp parallel
   	{
-  		int threadid = omp_get_thread_num();
-  		int count;
-  		#pragma omp for
-	  	for (i=0 ; i<niter; i++) {
+  		srand(SEED);
+  		int num_threads = omp_get_num_threads();
+		int threadidx = omp_get_thread_num();
+  		int count = 0;
+	  	for (i = 0 ; i < niter; i ++ ) {
 			x = (double)rand()/RAND_MAX;
 			y = (double)rand()/RAND_MAX;
 			z = x*x + y*y;
 			if (z<=1) count++;
 			}
 
-		#pragma omp atomic
-		sum += count;
+		carray[threadidx] = count;
 
 
 	}
 
-	pi=(double)sum/niter*4;
+	for( i = 0; i < 24; i++){
+		sum += carray[i];
+	}
+
+	pi = (double)sum/niter*4;
 	printf("# of trials= %d , estimate of pi is %g \n",niter,pi);
 	return 0;
 
